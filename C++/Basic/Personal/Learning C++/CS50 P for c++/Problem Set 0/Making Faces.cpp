a4a3928136410f1bede4ee4f6f932ba122031d2e
Aarav <athakur@dohacollege.com.qa>
2026-04-09 06:33:49 +0300

    Making Faces Finished and configure .exe file to save in folder

---
+++ C++/Basic/Personal/Learning C++/CS50 P for c++/Problem Set 0/Making Faces.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string convert(string c){
+    if (c == ")"){
+        return "🙂";
+    } else if (c == "("){
+        return "🙁";
+    } else{
+        return c;
+    }
+
+}
+
+int main(){
+    string message;
+    getline(cin, message);
+
+    for (int a = 0; a < message.length()-1; a++){
+        if (message.at(a) == ':'){
+            string new_string = convert(string (1, message.at(a+1)));
+            message.replace(a, 2, new_string);
+        }
+    }
+
+    cout << message << endl;
+    return 0;
+}
