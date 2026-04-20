56353288593739f15e33858c516ef8a8c54609e4
Aarav <athakur@dohacollege.com.qa>
2026-04-20 17:19:09 +0300

    Just file managment

---
+++ Basic/Learncpp/Chapter 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int inp_num;
+
+    std::cout << "Enter an Integer: ";
+    std::cin >> inp_num;
+    std::cout << "Double that number is: " << 2 * inp_num;
+
+    return 0;
+}
