cf604ffae3661663fa30a2c7a86ced616ce70034
Aarav <athakur@dohacollege.com.qa>
2026-05-15 09:11:12 +0300

    Learned how to use linked lists from problme 876

---
+++ Basic/LeetCode/Courses/1. Beginner's guide/Challenge Problems/876. Middle of Linked List.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+ ListNode* middleNode(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;}
+        ListNode* slow = head;
+        ListNode* fast = head;
+            
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+}
+
+int main(){
+    ListNode* ln = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+    ListNode* ans = middleNode(ln);
+    return 0;
+}
