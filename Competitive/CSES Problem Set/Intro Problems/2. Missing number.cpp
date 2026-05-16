e36031d1913772c4ba79323a802129ca799c1a87
Aarav <athakur@dohacollege.com.qa>
2026-05-16 09:18:03 +0300

    Revampped the new code to not use find

--- Competitive/CSES Problem Set/Intro Problems/2. Missing number.cpp
+++ Competitive/CSES Problem Set/Intro Problems/2. Missing number.cpp
@@ -1,35 +1,53 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 #include <string>
 using namespace std;
 using lln = long long int;
 
-int search(lln n, string& list){
+lln search(lln n, vector<lln> list){
+    vector<lln> lista;
+    lista.resize(n);
+    for (lln i = 1; i < n+1; i++){
+        lista[i-1] = i;
+    }
 
-    for (int i; i < n+1; i++){
-        string ref = " ";
-        ref.append(to_string(i));
-        ref.append(" ");
-        
-        long long int loc = list.find(ref);
-        if (loc < 0){
+    for(lln i: list){
+        lista[i-1] = 0;
+    }
+
+    for(lln i:lista){
+        if (i != 0){
             return i;
-        } else {
-            list.erase(loc, size(ref)-1);
         }
     }
-    return 0;
+}
+
+vector<lln> conversion(string input){
+    vector<lln> numbers;
+
+    stringstream ss(input);
+    int value;
+    // Extract integers one by one until the stream is exhausted
+    while (ss >> value) {
+        numbers.push_back(value);
+    }
+
+    // Print the vector to verify
+
+    return numbers;
+
 }
 
 int main(){
     string n;
     string tt;
-    //getline(cin, n);
-    //getline(cin, tt);
-
+    getline(cin, n);
+    getline(cin, tt);
+    vector<lln> c = conversion(tt);
     long long a = stoll(n);
-    int value = search(a, tt);
+    int value = search(a, c);
     cout << value;
     return 0;
 }
