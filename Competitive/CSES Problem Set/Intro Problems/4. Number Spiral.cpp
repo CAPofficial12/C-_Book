cf785427bbe70d1181ea226006fdc08f8814cf45
Aarav <athakur@dohacollege.com.qa>
2026-05-25 13:28:53 +0300

    Completed Number spiral.Pow() returns a double not an int

--- Competitive/CSES Problem Set/Intro Problems/4. Number Spiral.cpp
+++ Competitive/CSES Problem Set/Intro Problems/4. Number Spiral.cpp
@@ -9,7 +9,7 @@ inta Sol_square(inta x, inta y){
     //cin >> x;
     inta Max = max(x,y);
     inta Min = min(x,y);
-    inta square = pow(Max, 2) - Max + 1;
+    inta square = Max * Max - Max + 1;
     int diff = y-x;
     if (Max % 2 == 0){
         ans = square + diff;
@@ -26,18 +26,18 @@ inta Sol_Car(inta x, inta y){
     inta ans;
     if (Maxi == x){
         if (Maxi % 2 == 1){
-            top = pow(Maxi, 2);
+            top = Maxi * Maxi;
             ans = top - (y-1);
         } else{
-            top = pow(Maxi-1,2) + 1;
+            top = (Maxi-1) * (Maxi - 1) + 1;
             ans = top + (y-1);
         }
     } else{
         if (Maxi % 2 == 0){
-            side = pow(Maxi, 2);
+            side = Maxi * Maxi;
             ans = side - (x-1);
         } else{
-            side = pow(Maxi-1,2) + 1;
+            side = (Maxi-1) * (Maxi - 1) + 1;
             ans = side + (x-1);
         }
     }
@@ -45,19 +45,18 @@ inta Sol_Car(inta x, inta y){
 }
 
 int main(){
-    int tests = 1;
-    //cin >> tests;
+    int tests;
+    cin >> tests;
     for (inta i = 0; i < tests; i++){
             
         inta x;
         inta y;
+        string a;
         cin >> y;
         cin >> x;
-            
-        inta ans1 = Sol_Car(x,y);
-        inta ans2 = Sol_square(x,y);
-        cout << ans1 << " " << ans2;
-        cout << endl;
+    
+        inta ans1 = Sol_square(x,y);
+        cout << ans1 << endl;
     }
     return 0;
 }
