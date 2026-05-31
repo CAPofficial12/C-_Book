bdc503f6e836fc2fbf95671f16892f9839db0f95
Aarav <athakur@dohacollege.com.qa>
2026-05-31 22:55:09 +0300

    Finished Set structures

---
+++ Competitive/CSES Problem Set/0. Handbook/I. Basic Techniques/4. Data structures/4.2 Set structures/Sets.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_set>
+#include <set>
+using namespace std;
+
+int main(){
+    set<int> s;
+    s.insert(3);
+    s.insert(2);
+    s.insert(5);
+    cout << s.count(3) << "\n";
+
+    s.erase(3);
+    s.insert(4);
+    cout << s.count(4) << "\n";
+
+    cout << s.size() << "\n";
+
+    for (auto x : s) {
+        s.insert(5); //Multiple inserts will not change anything so count oly return if numbrt is present or not
+        cout << x << " ";
+    }
+    cout << "\n";
+
+    multiset<int> Sm;
+    Sm.insert(5);
+    Sm.insert(5);
+    Sm.insert(5);
+    cout << Sm.count(5) << " "; //Multi set permits multiple instances
+
+    Sm.erase(Sm.find(5));   //Removes only a single instance
+    cout << Sm.count(5) << " ";
+
+    Sm.erase(5);
+    cout << s.count(5) << " "; //Erase removes all instance of variable
+}
