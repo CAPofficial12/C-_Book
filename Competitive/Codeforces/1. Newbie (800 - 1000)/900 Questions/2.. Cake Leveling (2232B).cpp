72f4757801d7b789ebd3669e67124ef355f20200
Aarav <athakur@dohacollege.com.qa>
2026-06-10 22:31:42 +0300

    Finished Codeforces 2232B: Cove leveling

---
+++ Competitive/Codeforces/1. Newbie (800 - 1000)/900 Questions/2.. Cake Leveling (2232B).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        long long pref = 0;
+        long long mn = LLONG_MAX;
+
+        for (int i = 1; i <= n; i++) {
+            long long x;
+            cin >> x;
+
+            pref += x;
+            mn = min(mn, pref / i);
+
+            cout << mn;
+            if (i < n) cout << ' ';
+        }
+        cout << '\n';
+    }
+}
