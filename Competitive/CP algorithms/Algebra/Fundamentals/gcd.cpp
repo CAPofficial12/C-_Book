c1334f5ddb03e327c8437ffdb3f650b9ff6c1369
Aarav <athakur@dohacollege.com.qa>
2026-06-28 15:37:36 +0300

    Read about Euclidean GCD calculator

--- Competitive/CP algorithms/Algebra/Fundamentals/gcd.cpp
+++ Competitive/CP algorithms/Algebra/Fundamentals/gcd.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int a, int b);
+int E_gcd(int a, int b);
 
 int main(){
     int a, b;
     cin >> a;
     cin >> b;
-    int g = gcd(a, b);
+    int g = E_gcd(a, b);
     cout << g;
     return 0;
 }
 
-int gcd(int a, int b){
+int E_gcd(int a, int b){
     while (b > 0){
         a %= b;
         swap(a,b);
