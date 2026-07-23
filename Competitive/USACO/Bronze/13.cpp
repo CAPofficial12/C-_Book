8fe6ac423ef4e6bf0cca44e0453d23707ec5b0c5
Aarav <athakur@dohacollege.com.qa>
2026-07-23 15:11:35 +0300

    I still dont understand how to do Sleeping in class

--- Competitive/USACO/Bronze/13.cpp
+++ Competitive/USACO/Bronze/13.cpp
@@ -1,59 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long int;
 
-bool check(vector<ll> reference){
-    ll r = reference[0];
-    for(auto c:reference){
-        if (r != c){
-            return false;
-        }
-    }
-    return true;
-}
-
-int main(){
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    ll T = 1;
-    cin >> T;
+	int test_num;
+	cin >> test_num;
+	for (int t = 0; t < test_num; t++) {
+		int n;
+		cin >> n;
+		vector<int> elsie_log = vector<int>(n);
+		int log_sum = 0;
+		for (int &h : elsie_log) {
+			cin >> h;
+			log_sum += h;
+		}
 
-    for(ll j = 0; j < T; j++){
+		if (log_sum == 0) {
+			cout << 0 << '\n';
+			continue;
+		}
 
-        ll n;
-        cin >> n;
-        
-        vector<ll> cl (n);
-        ll max = 0;
-        for(auto& c: cl){
-            cin >> c;
-            if (c > max){
-                max = c;
-            }
-        }
+		auto valid = [&](int num_hours) {
+			int curr_sum = 0;  // The current number of hours Elsie's logging
+			for (int h : elsie_log) {
+				curr_sum += h;
+				if (curr_sum > num_hours) {
+					return false;  // curr_sum can't exceed num_hours
+				} else if (curr_sum == num_hours) {
+					curr_sum = 0;
+				}
+			}
+			return true;
+		};
 
-        ll steps = 0;
-        while(!check(cl)){
-            for(ll i = 0; i < cl.size()-1;i++){
-                if (cl[i] < max){
-                    cl[i] +=  cl[i + 1];
-                    cl.erase(cl.begin() + i+1);
-                    steps += 1;
-                }  if (cl[i] > max){
-                    max = cl[i];
-                }
+		// Try possible number of hours after modification in increasing order.
+		bool found = false;
+		int max_factor = 0;
+		for (int factor = 1; factor * factor <= log_sum; factor++) {
+			max_factor = factor;
+			if (log_sum % factor == 0 && valid(factor)) {
+				// log_sum/factor is the total # of classes AFTER modifying
+				cout << n - log_sum / factor << '\n';
+				found = true;
+				break;
+			}
+		}
+		if (found) { continue; }
 
-                if (cl.size() == 2){
-                    cl[i] +=  cl[i + 1];
-                    cl.erase(cl.begin() + i+1);
-                    steps += 1;
-                }
-
-            }
-        }
-        cout << steps << "\n";
-    }
-
-    return 0;
+		for (int factor = max_factor; factor >= 1; factor--) {
+			if (log_sum % factor == 0 && factor * factor != log_sum) {
+				int num_hours = log_sum / factor;
+				if (valid(num_hours)) {
+					// log_sum/num_hours is the total # of classes AFTER modifying
+					cout << n - log_sum / num_hours << '\n';
+					break;
+				}
+			}
+		}
+	}
 }
