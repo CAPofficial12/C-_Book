f54fedfc958907a7228dd65208cc5fefb87f21cd
Aarav <athakur@dohacollege.com.qa>
2026-08-08 11:53:59 +0530

    Attempted Problem G QCPC 2025: Maximise and Count

---
+++ Competitive/QCPC/Contest/Problem G. Maximise AND Count.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    set<ll> S = {};
+    ll Q = 0;
+    cin >> Q;
+    for(int i = 0; i < Q; i++){
+        ll t, x;
+        cin >> t >> x;
+        if (t == 1){
+            S.insert(x);
+        } else if (t == 2){
+            S.erase(x);
+        } else if (t==3){
+            ll count = 0;
+            ll maximum = 0;
+            ll ad = 0;
+            for(ll a:S){
+                maximum = max(maximum, x&a);
+            }
+            for(ll a:S){
+                if (maximum != x&a){
+                    count ++;
+                }
+            }
+            cout << maximum << " " << count << endl;
+        }
+    }
+
+    return 0;
+}
