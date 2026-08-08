98a852e5cef7c79f0ee0598a8253cbd8618f1c91
Aarav <athakur@dohacollege.com.qa>
2026-08-08 11:52:53 +0530

    Completed I Hate Minimums QCPC

--- Competitive/QCPC/Contest/Problem A. I hate minimums.cpp
+++ Competitive/QCPC/Contest/Problem A. I hate minimums.cpp
@@ -3,8 +3,8 @@ using namespace std;
 using ll = long long int;
 
 int main(){
-    //ios::sync_with_stdio(0);
-    //cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     ll n;
     cin >> n;
