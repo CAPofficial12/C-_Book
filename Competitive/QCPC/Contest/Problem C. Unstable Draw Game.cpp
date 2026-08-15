aae5c8ebd334a9ea94f4c468005b6efbf7321fd4
Aarav <athakur@dohacollege.com.qa>
2026-08-15 09:41:46 +0530

    Completed Unstable Draw Game

---
+++ Competitive/QCPC/Contest/Problem C. Unstable Draw Game.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+
+int main(){
+    ll t;
+    cin >> t;
+    ll turn = 0;
+    for(ll i = 0; i < t; i++){
+        ll n;
+        cin >> n;
+        cout << "Anne";
+        
+    }
+    return 0;
+}
